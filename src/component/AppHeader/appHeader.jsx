0677f37506aac6b98aaf2597df04f27342d916d6
Anna <anna_p@rupor.agency>
2022-09-28 22:34:59 +0300

    added appHeader component

---
+++ src/component/AppHeader/appHeader.jsx
@@ -0,0 +1,43 @@
+import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
+import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import LinkItem from '../LinkItem/LinkItem'
+
+import './appHeader.scss';
+
+const AppHeader = () => {
+    return(
+        <header className='header'>
+            <div className='pt-5'>
+                <Logo />
+            </div>
+            
+            <nav className='mt-4'>
+                <div className='navbar-item'>
+                    <LinkItem class='navbar-item__link mr-2 p-4 navbar-item__link_active'>
+                        <BurgerIcon type="primary" />
+                        <p className="text text_type_main-default ml-2">Конструктор</p>
+                    </LinkItem>
+                    <LinkItem class='navbar-item__link mr-2 p-4 active'>
+                        <ListIcon type="primary"/>
+                        <p className="text text_type_main-default ml-2">Лента заказов</p>
+                        
+                    </LinkItem>
+                </div>
+
+                <div>
+                    <LinkItem class='navbar-item__link mr-2 p-4 active'>
+                        <ProfileIcon type="primary"/>
+                        
+                        <p className="text text_type_main-default ml-2">Личный кабинет</p>
+
+                    </LinkItem>
+                </div>
+            </nav>
+            
+        </header>
+    )
+}
+
+export default AppHeader;
--- src/component/app/App.tsx
+++ src/component/app/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import AppHeader from '../AppHeader/appHeader';
 import './App.css';
 
 function App() {
   return (
     <main className="App">
-      Hellow world!
+      <AppHeader/>
     </main>
   );
 }
