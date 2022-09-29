52c0ebad355c899f1f63f6ffc7d81320fd40de0d
Anna <anna_p@rupor.agency>
2022-09-29 20:49:16 +0300

    added LinkItem component

--- src/component/LinkItem/LinkItem.js
+++ src/component/LinkItem/LinkItem.js
@@ -1,11 +1,10 @@
+import React from 'react'
 
 const LinkItem = (props) => {
     return(
         <a className={props.class} href="#">
             {props.children}
-        </a>
-            
-       
+        </a>  
     )
 }
 
