5fa652180920ab89a5b034237e2f3c60ede53069
Anna <anna_p@rupor.agency>
2022-09-29 20:31:57 +0300

    added Navbar component

--- src/component/AppHeader/appHeader.js
+++ src/component/AppHeader/appHeader.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Logo } from '@ya.praktikum/react-developer-burger-ui-components'
 import { BurgerIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components'
@@ -7,36 +8,43 @@ import LinkItem from '../LinkItem/LinkItem'
 import './appHeader.scss';
 
 const AppHeader = () => {
-    return(
+    return (
         <header className='header'>
             <div className='pt-5'>
                 <Logo />
             </div>
-            
-            <nav className='mt-4'>
-                <div className='navbar-item'>
-                    <LinkItem class='navbar-item__link mr-2 p-4 navbar-item__link_active'>
-                        <BurgerIcon type="primary" />
-                        <p className="text text_type_main-default ml-2">Конструктор</p>
-                    </LinkItem>
-                    <LinkItem class='navbar-item__link mr-2 p-4 active'>
-                        <ListIcon type="primary"/>
-                        <p className="text text_type_main-default ml-2">Лента заказов</p>
-                        
-                    </LinkItem>
-                </div>
 
-                <div>
-                    <LinkItem class='navbar-item__link mr-2 p-4 active'>
-                        <ProfileIcon type="primary"/>
-                        
-                        <p className="text text_type_main-default ml-2">Личный кабинет</p>
+            <Navbar />
 
-                    </LinkItem>
-                </div>
-            </nav>
-            
+
         </header>
+    )
+}
+
+const Navbar = () => {
+    return (
+        <nav className='mt-4'>
+            <div className='navbar-item'>
+                <LinkItem class='navbar-item__link mr-2 p-4 navbar-item__link_active'>
+                    <BurgerIcon type="primary" />
+                    <p className="text text_type_main-default ml-2">Конструктор</p>
+                </LinkItem>
+                <LinkItem class='navbar-item__link mr-2 p-4 active'>
+                    <ListIcon type="primary" />
+                    <p className="text text_type_main-default ml-2">Лента заказов</p>
+
+                </LinkItem>
+            </div>
+
+            <div>
+                <LinkItem class='navbar-item__link mr-2 p-4 active'>
+                    <ProfileIcon type="primary" />
+
+                    <p className="text text_type_main-default ml-2">Личный кабинет</p>
+
+                </LinkItem>
+            </div>
+        </nav>
     )
 }
 
