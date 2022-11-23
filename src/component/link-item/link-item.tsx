1491c8a2dc92b7367dd8eed18a5d8dd0f27ff8ff
AnnPak <pandaqwerty1357@yandex.ru>
2022-11-23 19:40:17 +0300

    fixed pages routing

--- src/component/link-item/link-item.tsx
+++ src/component/link-item/link-item.tsx
@@ -1,12 +1,13 @@
 import { FC, PropsWithChildren  } from "react";
+import { Link } from "react-router-dom";
 
 import { TLinkItem } from "../../utils/types";
 
 const LinkItem: FC<PropsWithChildren<TLinkItem>> = ({ className, to, children }) => {
     return (
-        <a className={className} href={to}>
+        <Link className={className} to={to}>
             {children}
-        </a>
+        </Link>
     );
 };
 
