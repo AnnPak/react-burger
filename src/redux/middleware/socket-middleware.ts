04d44622d99b5825bd0f69d8f86a97b2db2c9ef6
AnnPak <pandaqwerty1357@yandex.ru>
2022-12-04 13:58:45 +0300

    added websocket middleware

---
+++ src/redux/middleware/socket-middleware.ts
@@ -0,0 +1,75 @@
+import { RootState, AppDispatch } from '../store/index';
+// socketMiddleware.ts
+import type { Middleware, MiddlewareAPI } from 'redux';
+import { ActionCreatorWithoutPayload, ActionCreatorWithPayload } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
+import { wsMessage } from '../store/feed/slice';
+// import type { AppActions, AppDispatch, RootState } from '../types';
+
+export enum wsActionType {
+  wsConnect = 'wsConnect',
+  wsDisconnect = 'wsDisconnect',
+  wsConnecting = 'wsConnecting',
+  wsOpen = 'wsOpen',
+  wsClose = 'wsClose',
+  wsError = 'wsError',
+  wsMessage = 'wsMessage',
+}
+
+export type TwsActionTypes = {
+  wsConnect: ActionCreatorWithPayload<string>,
+  wsDisconnect: ActionCreatorWithoutPayload,
+  wsConnecting: ActionCreatorWithoutPayload,
+  wsOpen: ActionCreatorWithoutPayload,
+  wsClose: ActionCreatorWithoutPayload,
+  wsError: ActionCreatorWithPayload<string>,
+  wsMessage: ActionCreatorWithPayload<string>,
+}
+
+export const socketMiddleware = (wsUrl: string): Middleware => {
+    return ((store: MiddlewareAPI<AppDispatch, RootState>) => {
+        let socket: WebSocket | null = null;
+
+    return next => (action) => {
+      const { dispatch, getState } = store;
+      const { type, payload } = action;
+ 
+      if (type === 'wsConnecting') {
+            // объект класса WebSocket
+
+        socket = new WebSocket(wsUrl);
+      }
+      if (socket) {
+
+        // функция, которая вызывается при открытии сокета
+        socket.onopen = event => {
+          dispatch({ type: 'wsConnect', payload: event });
+        };
+
+                // функция, которая вызывается при ошибке соединения
+        socket.onerror = event => {
+          dispatch({ type: 'wsError', payload: event });
+        };
+
+                // функция, которая вызывается при получения события от сервера
+        socket.onmessage = event => {
+          const { data: serializedData } = event;
+          const data = JSON.parse(serializedData);
+          dispatch(wsMessage(data));
+        };
+                // функция, которая вызывается при закрытии соединения
+        socket.onclose = event => {
+          dispatch({ type: 'wsClose', payload: event });
+        };
+
+        if (type === 'wsMessage') {
+          const message = payload;
+                    // функция для отправки сообщения на сервер
+          socket.send(JSON.stringify(message));
+        }
+      }
+
+      next(action);
+    };
+    }) as Middleware;
+}; 
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 
 import burgerConstructor from "./constructor/slice";
 import ingredients from "./ingredients/slice";
@@ -8,14 +8,21 @@ import user from "./user/user";
 import logout from "./user/logout";
 import register from "./user/register";
 import password from "./user/password";
+import feed from "./feed/slice";
+import { socketMiddleware } from "../middleware/socket-middleware";
+import { API_HOST_URL } from "../../utils/constants";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const store = configureStore({
-    reducer: { burgerConstructor, ingredients, modal, order, user, register, password, logout},
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    reducer: { burgerConstructor, ingredients, modal, order, user, register, password, logout, feed},
+    middleware: getDefaultMiddleware({serializableCheck: false}).concat(socketMiddleware('wss://norma.nomoreparties.space/orders/all')),
     devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
