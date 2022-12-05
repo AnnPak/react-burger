e945041807e0f19e88174c377dcf8ba7ff1b6af7
AnnPak <pandaqwerty1357@yandex.ru>
2022-12-05 20:49:59 +0300

    fixed profile order page

--- src/pages/profile/profile-orders.tsx
+++ src/pages/profile/profile-orders.tsx
@@ -1,10 +1,20 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import OrdersList from "../../component/orders-list/orders-list";
 import Preloader from "../../component/preloader/preloader";
-import { RootState, useAppSelector } from "../../redux/store";
+import { wsActionType } from "../../redux/middleware/socket-middleware";
+import { RootState, useAppDispatch, useAppSelector } from "../../redux/store";
 
 const Orders: FC = () => {
     const { userOrders } = useAppSelector((store: RootState) => store.feed);
+    const dispatch = useAppDispatch()
+    
+    useEffect(() => {
+        dispatch({ type: wsActionType.wsConnecting });
+        return () => {
+            dispatch({ type: wsActionType.wsClose });
+        };
+        // eslint-disable-next-line
+    }, []);
 
     return (
         <>
